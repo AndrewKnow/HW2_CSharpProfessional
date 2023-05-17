7b873a12a40289829e9ac1333727283ecb990879
Андрей Наумов <NaumovAV1983@yandex.ru>
2023-05-17 17:31:03 +0300

    Разработка 2

--- HW2_CSharpProfessional/CreateTables.cs
+++ HW2_CSharpProfessional/CreateTables.cs
@@ -9,12 +9,76 @@ namespace HW2_CSharpProfessional
         {
             // Проверка развертывания базы по таблице salesman
             var canCreate = CanCreateBase();
+
             if (canCreate) 
             {
                 // Создание базы "авито"
-                CreateTableSalesman();
-                CreateTableProduct();
-                CreateTableBuyer();
+                var connectionString = DBConnection.ConnectionString();
+                using var connection = new NpgsqlConnection(connectionString);
+                connection.Open();
+
+                var sql1 = @"
+                        CREATE SEQUENCE salesman_id_seq;
+
+                        CREATE TABLE salesman
+                        (
+                            id              BIGINT                      NOT NULL    DEFAULT NEXTVAL('salesman_id_seq'),
+                            shop            CHARACTER VARYING(255)      NOT NULL,
+                          
+                            CONSTRAINT salesman_pkey PRIMARY KEY (id),
+                            CONSTRAINT salesman_product_unique UNIQUE (product)
+                        );
+                        
+                        CREATE INDEX salesman_shop_idx ON salesman(shop);
+                        ";
+
+                using var cmd1 = new NpgsqlCommand(sql1, connection);
+                cmd1.ExecuteNonQuery();
+
+                var sql2 = @"
+                        CREATE SEQUENCE product_id_seq;
+                        
+                        CREATE TABLE product
+                        (
+                            id              BIGINT                      NOT NULL    DEFAULT NEXTVAL('product_id_seq'),
+                            salesman_id     BIGINT                      NOT NULL,  
+                            name            CHARACTER VARYING(255)      NOT NULL,
+                            price           MONEY                       NOT NULL,
+                            count           INTEGER                     NOT NULL,
+
+                            CONSTRAINT product_pkey PRIMARY KEY (id),
+                            CONSTRAINT product_fk_salesman_id FOREIGN KEY (salesman_id) REFERENCES salesman(id) ON DELETE CASCADE
+                        );
+
+                        CREATE INDEX product_name_idx ON product(name);
+                        ";
+
+                using var cmd2 = new NpgsqlCommand(sql2, connection);
+                cmd2.ExecuteNonQuery();
+
+
+                var sql3 = @"
+                        CREATE SEQUENCE buyer_id_seq;
+                        
+                        CREATE TABLE product
+                        (
+                            id              BIGINT                      NOT NULL    DEFAULT NEXTVAL('buyer_id_seq'),
+                            first_name      CHARACTER VARYING(255)      NOT NULL,
+                            second_name     CHARACTER VARYING(255)      NOT NULL,
+                            email           CHARACTER VARYING(255)      NOT NULL,
+                            product_id      BIGINT                      NOT NULL,  
+                            count           INTEGER                     NOT NULL,
+
+                            CONSTRAINT buyer_pkey PRIMARY KEY (id),
+                            CONSTRAINT buyer_fk_product_id FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE SET NULL
+                        );
+                        CREATE UNIQUE INDEX clients_email_unq_idx ON clients(lower(email));
+                        ";
+
+                using var cmd3 = new NpgsqlCommand(sql3, connection);
+                cmd3.ExecuteNonQuery();
+
+                Console.WriteLine("Таблицы созданы");
             }
         }
 
@@ -39,48 +103,5 @@ namespace HW2_CSharpProfessional
                 return true;
             }    
         }
-
-
-        static void CreateTableSalesman()
-        {
-            var connectionString = DBConnection.ConnectionString();
-            using var connection = new NpgsqlConnection(connectionString);
-            connection.Open();
-
-            var sql = @"
-                        CREATE SEQUENCE salesman_id_seq;
-
-                        CREATE TABLE salesman
-                        (
-                            id              BIGINT                      NOT NULL    DEFAULT NEXTVAL('salesman_id_seq'),
-                            shop            CHARACTER VARYING(255)      NOT NULL,
-                            product         CHARACTER VARYING(255)      NOT NULL,
-                            price           MONEY                       NOT NULL,
-                            count           INTEGER                     NOT NULL,
-                          
-                            CONSTRAINT salesman_pkey PRIMARY KEY (id),
-                            CONSTRAINT salesman_product_unique UNIQUE (product)
-                        );
-                        
-                        CREATE INDEX salesman_product_idx ON salesman(product);
-                        ";
-
-            using var cmd = new NpgsqlCommand(sql, connection);
-
-            var affectedRowsCount = cmd.ExecuteNonQuery().ToString();
-
-            Console.WriteLine("Создана таблица SALESMAN");
-
-        }
-
-        static void CreateTableProduct()
-        {
-
-        }
-        static void CreateTableBuyer()
-        {
-
-        }
-
     }
 }
